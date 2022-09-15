77dc709870640600a698305bbd90217ece06acf2
Angel Duarte <esdrexd.106@gmail.com>
2022-09-15 09:54:11 -0600

    Clase 1 unidad 4

---
+++ unidad 4/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <link rel="stylesheet" href="public/css/main.css">
+</head>
+<body>
+    <div class="container">
+
+        <form action="">
+            <fieldset>
+                <legend>Datos de acceso</legend>
+                <label>
+                    Email
+                </label>
+                <input type="text" placeholder="Escribe">
+                <label>
+                    Password
+                </label>
+                <input type="password" placeholder="* * * * * *">
+                <button type="submit">
+                    Enviar
+                </button>
+            </fieldset>
+        </form>
+
+    </div>
+</body>
+</html>
