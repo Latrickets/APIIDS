673caaca8e39389f8139d05c88091caab401c44c
Angel Duarte <esdrexd.106@gmail.com>
2022-09-20 10:00:37 -0600

    login bootstrap ejemplo

--- unidad 4/index.php
+++ unidad 4/index.php
@@ -5,28 +5,38 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceso al panel</title>
-    <link rel="stylesheet" href="public/css/main.css">
+    <!-- <link rel="stylesheet" href="public/css/main.css"> -->
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
+    <div class="container bg-light">
+        <div class="row align-items-center">
+            <div class="col-md-4">
 
-        <form action="">
-            <fieldset>
-                <legend>Datos de acceso</legend>
-                <label>
-                    Email
-                </label>
-                <input type="text" placeholder="Escribe">
-                <label>
-                    Password
-                </label>
-                <input type="password" placeholder="* * * * * *">
-                <button type="submit">
-                    Enviar
-                </button>
-            </fieldset>
-        </form>
-
+            </div>
+            <div class="col">            
+                <form action="">
+                    <fieldset>
+                        <legend>Datos de acceso</legend>
+                        
+                        <label>
+                            Email
+                        </label>
+                        <input type="text" placeholder="Escribe">
+                        <br>
+                        <label>
+                            Password
+                        </label>
+                        <input type="password" placeholder="* * * * * *">
+                        <br><br>
+                        <button class="btn btn-secondary" type="submit">
+                            Enviar
+                        </button>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
