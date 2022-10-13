65e07fc23909387390bc7f9ddd904a23594e615c
Angel Duarte <esdrexd.106@gmail.com>
2022-10-13 09:45:50 -0600

    inicio laravel rutas

---
+++ reservaciones/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/saludo', function(){
+    echo 'hola';
+});
+
+Route::get('/saludo/{name}', function($name){
+    echo 'hola '.$name;
+});
+
+Route::get('/suma/{num1}/{num2}/{num3?}', function($num1,$num2,$num3 = 0){
+    $res = $num1 + $num2 + $num3;
+    echo 'La suma es: ' . $res;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+
+Route::post('/suma', function (Request $request) {
+    
+});
