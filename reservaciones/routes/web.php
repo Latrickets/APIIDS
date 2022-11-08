46236b60f896e3f76190bd2c86661474013079e9
Angel Duarte <esdrexd.106@gmail.com>
2022-11-08 09:50:49 -0700

    actividad 22

--- reservaciones/routes/web.php
+++ reservaciones/routes/web.php
@@ -20,6 +20,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('prueba', function () {
+    return 'hola';
+})->middleware('auth');
 Route::get("users/", [UserController::class, "index"]);
 Route::get("users/create", [UserController::class, "create"]);
 Route::get("users/{id}", [UserController::class, "show"]);
